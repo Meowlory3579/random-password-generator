3124b3d03ded4425d21ed397f8facc7ad60d0ea5
Mallory Howard <mallory1313@msn.com>
2024-01-12 18:45:40 -0700

    updated README, corrected css ref link in HTML, finished code in JavaScipt

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-// Begin: establish variables for user inputs
+// Begin: create variables for user selections
 var passwordLength = 0 ;
 var includeLowercase = null;
 var includeUppercase = null;
 var includeNumeric = null;
 var includeSymbols = null;
-var options = [];
-// End: establish variables for user inputs
+var confirmSelections = '';
+// End: create variables for user selections
 
 
-// Begin: specify values to be included for each variable
+// Begin: specify array values to be included based on user selections
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbols = ["!", "@", "#", "*", "&"];
-// End: specify values to be included for each variable
+var symbols = ["!", "@", "#", "*", "&", "$", "%", "+", ":", ";", "=", "?", "^", "_", "-", "~"];
+var options = [];
+// End: specify array values to be included based on user selections
 
 
 // Start function to generate password
@@ -30,21 +31,27 @@ function generatePassword () {
     includeUppercase = confirm("Include Uppercase?\nSelect 'OK' for 'Yes', 'Cancel' for 'No'.");
     includeNumeric = confirm("Include Numbers?\nSelect 'OK' for 'Yes', 'Cancel' for 'No'.");
     includeSymbols = confirm("Include Symbols?\nSelect 'OK' for 'Yes', 'Cancel' for 'No'.");
-  };
+  }
 
-  // Check that at least one "include" option is made, present alert if not & return null so generatePassword() != "undefined"
+   // Check that at least one "include" option is made, present alert if not & return null so generatePassword() != "undefined"
   if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSymbols) {
-    alert("You must make at least one selection.");
+    alert("You must include at least one option: Lowercase, Uppercase, Numbers, and/or Symbols.");
     return null;
-  };
+  } 
 
-  // Build new array based on options selected
-  if (includeLowercase && includeUppercase && includeNumeric && includeSymbols) {
-    options = lowercase.concat(uppercase, numeric, symbols)
-  } else if (includeLowercase && includeUppercase && includeNumeric) {
-    options = lowercase.concat(uppercase, numeric)
-  };
-
+  // Build new array based on options the user selects
+  if (includeLowercase) {
+    options.push(...lowercase);
+  }
+  if (includeUppercase) {
+   options.push(...uppercase);
+  }
+  if (includeNumeric) {
+   options.push(...numeric);
+  }
+  if (includeSymbols) {
+    options.push(...symbols);
+  }
 
   // Once options array is built, extract random data from it and put into password string until it's the same length as passwordLength
   var passwordString = "";
@@ -53,19 +60,12 @@ function generatePassword () {
     passwordString += options[index];
   }
 
-  // Return the generated password, so it gets placed in generatePassword() function
+  // Return the generated passwordString, so it gets placed in generatePassword() function
   return passwordString;
-
-  console.log(passwordLength);
-  console.log(options);
-  console.log(passwordString);
 
 }
 // End function to generate password
 
-
-
-// DON'T TOUCH!!!!
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
